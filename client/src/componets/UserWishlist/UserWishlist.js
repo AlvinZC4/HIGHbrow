8a0f7a76a46a63a72b8f3eb1b6af480d0ab7b927
Alvin <alvinzcoxiv@gmail.com>
2020-10-29 22:29:21 -0400

    created UserWishlist component

---
+++ client/src/componets/UserWishlist/UserWishlist.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+function UserWishlist(props) {
+    return (
+        <div className="list-overflow-container">
+      <ul className="list-group">{children}</ul>
+    </div>
+    )
+}
+
+export default UserWishlist
