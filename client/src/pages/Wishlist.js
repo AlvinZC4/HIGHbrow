4749e6aefb7c29ce8d1684146ffb633a7c4f2c55
Alvin <alvinzcoxiv@gmail.com>
2020-10-29 22:43:07 -0400

    added components for wishlist page

--- client/src/pages/Wishlist.js
+++ client/src/pages/Wishlist.js
@@ -1,4 +1,7 @@
 import React, {useState, useEffect} from "react"
+import UserWishlist from "../componets/UserWishlist"
+import WishlistItem from "../componets/WishlistItem"
+import API from "../utils/API"
 
 function Wishlist(props) {
     const [myBooks, setMyBooks] = useState({
