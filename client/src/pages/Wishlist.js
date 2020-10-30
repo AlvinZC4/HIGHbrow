848c680ccdd461db4ad3943ae2a0b5378ffeada6
Alvin <alvinzcoxiv@gmail.com>
2020-10-29 22:16:34 -0400

    began coding state in wishlist page

---
+++ client/src/pages/Wishlist.js
@@ -0,0 +1,13 @@
+import React, {useState, useEffect} from "react"
+
+function Wishlist(props) {
+    const [myBooks, setMyBooks] = useState({
+        id: "",
+        title: "",
+        author: "",
+        wishlist: false,
+        shared: false
+    })
+}
+
+export default Wishlist
