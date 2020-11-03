c95645b97a6c2ec92e30ea439a46613e69fc5c69
Alvin <alvinzcoxiv@gmail.com>
2020-11-03 18:10:30 -0500

    api response from GoodReads now only returns information we want to the front-end

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -1,18 +1,9 @@
 require('dotenv').config()
 const axios = require("axios");
-const User = require("../models/users");
 const parseString = require('xml2js').parseString;
-
-
-
-
-
-require('dotenv').config();
-
 
 const BASEURL = "https://www.goodreads.com/search.xml?&q=";
 const APIKEY = process.env.APIKEY;
-// const format = "json";
 
 // Export an object with a "search" method that searches the Giphy API for the passed query
 module.exports = {
@@ -24,116 +15,31 @@ module.exports = {
                 console.log("API Results", result)
                 parseString(result.data, function (err, result) {
                     //get only data you need array
-                    res.json(result);
+                    // The results we want are buried deep witin the API response, set resultArray to an array that gets to the resutls we are after
+                    const resultArray = result.GoodreadsResponse.search[0].results[0].work
+
+                    // map through the resultsArray to create a new array, called responseArray, that contains only the information we need for each book
+                    const responseArray = resultArray.map((index) => {
+                        return (
+                          {
+                            id: index.best_book[0].id[0]._,
+                            title: index.best_book[0].title[0],
+                            author: index.best_book[0].author[0].name[0]
+                          }  
+                        )
+                    })
+
+                    // send the responseArray back to the front-end
+                    res.json(responseArray);
 
                 });
 
             })
-            .catch(err => res.status(422).json(err))
+            .catch(err => {
+                res.status(422).json(err)
+                console.log(err)
+            })
 
     }
 
 }
-
-    // // add to db when add button clicked
-    // create: async (request, h) => {
-    //     const result = await User.create({  ///need to add book???
-    //         date: new Date(),
-    //         title: request.payload.noteTitle,
-    //         slug: Slugify(request.payload.noteTitle, { lower: true }),
-    //         description: request.payload.noteDescription,
-    //         content: request.payload.noteContent
-    //     });
-
-    //     // Generate a new note with the 'result' data
-    //     return result;
-    // }
-
-
-
-
-
-
-
-// const fs = require("fs");
-// const axios = require("axios");
-
-
-// const searchParam = "";
-
-// axios.get('/books?ID=12345')
-//     .then(function (response) {
-//         // handle success
-//         console.log(response);
-//     })
-//     .catch(function (error) {
-//         // handle error
-//         console.log(error);
-//     })
-//     .then(function () {
-//         // always executed
-//     });
-
-// // Optionally the request above could also be done as
-// axios.get('/books', {
-//     params: {
-//         ID: 12345
-//     }
-// })
-//     .then(function (response) {
-//         console.log(response);
-//     })
-//     .catch(function (error) {
-//         console.log(error);
-//     })
-//     .then(function () {
-//         // always executed
-//     });
-
-
-
-
-
-
-// const searchparam = "Harry+Potter";
-// axios({
-//     method: 'get',
-//     key: "eG089CXx7XHIGGeBeEKLg",
-//     // searchparam: "Harry+Potter",
-//     url: 'eG089CXx7XHIGGeBeEKLg' + key + "&q=" + searchparam,
-//     responseType: 'stream'
-// })
-//     .then(function (response) {
-//         response.data;
-
-//     });
-
-
-
-// //
-
-// export default {
-//     getSearchResults: function () {
-//         return axios({
-//             method: 'get',
-//             key: "eG089CXx7XHIGGeBeEKLg",
-//             searchparam: "Harry+Potter",
-//             url: 'https://www.goodreads.com/search.xml?key=' + key + "&q=" + searchparam,
-//             responseType: 'stream'
-//         }).then(function (response) {
-//             response.data.pipe(fs.createWriteStream('ada_lovelace.jpg'))
-//         });
-//     };
-// };
-
-
-// //https://www.goodreads.com/search.xml?key=eG089CXx7XHIGGeBeEKLg&q=Harry+Potter
-
-
-// export default {
-//     fetchUsers: function () {
-//         return axios.get(
-//             "https://randomuser.me/api/?results=50&nat=us&inc=id,name,email,gender,phone,picture"
-//         );
-//     },
-// };
