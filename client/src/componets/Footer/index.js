47857ab3d1bb0a82bc84c316dc3c04b6741f2e06
Marcia Paige <mapaige7@gmail.com>
2020-11-06 23:07:00 -0500

    Added className and switch links in navbar

--- client/src/componets/Footer/index.js
+++ client/src/componets/Footer/index.js
@@ -1,30 +1,29 @@
 import React from 'react';
-import "./styles.css"
+import './styles.css';
 
 function Footer() {
 	return (
 		<>
-			<footer class="footer mt-auto py-4">
-				<div class="container">
-					<div class="row align-items-center">
-						<div class="col-lg-4 text-lg-left">
-							Created by Alvin Cox | Marcia Paige | Lisa Survana
-							 Copyright ©2020
+			<footer className="footer mt-auto py-4">
+				<div className="container">
+					<div className="row align-items-center">
+						<div className="col-lg-4 text-lg-left">
+							Created by Alvin Cox | Marcia Paige | Lisa Survana Copyright ©2020
 							HIGHbrow
 						</div>
-						<div class="col-lg-4 my-3 my-lg-0">
-							<a class="btn btn-success btn-social mx-2" href="#!">
-								<i class="fab fa-twitter"></i>
+						<div className="col-lg-4 my-3 my-lg-0">
+							<a className="btn btn-success btn-social mx-2" href="#!">
+								<i className="fab fa-twitter"></i>
 							</a>
-							<a class="btn btn-success btn-social mx-2" href="#!">
-								<i class="fab fa-facebook-f"></i>
+							<a className="btn btn-success btn-social mx-2" href="#!">
+								<i className="fab fa-facebook-f"></i>
 							</a>
-							<a class="btn btn-success btn-social mx-2" href="#!">
-								<i class="fab fa-linkedin-in"></i>
+							<a className="btn btn-success btn-social mx-2" href="#!">
+								<i className="fab fa-linkedin-in"></i>
 							</a>
 						</div>
-						<div class="col-lg-4 text-lg-right">
-							<a class="mr-3" href="#!">
+						<div className="col-lg-4 text-lg-right">
+							<a className="mr-3" href="#!">
 								Privacy Policy
 							</a>
 							<a href="#!">Terms of Use</a>
